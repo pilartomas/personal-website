c4eb056cb7b4e0245959d1a263959c5be552eaa5
Tomas Pilar <tomasp@salsitasoft.com>
2020-10-23 19:41:14 +0200

    Introduce Particles library

--- src/Background.tsx
+++ src/Background.tsx
@@ -1,15 +1,7 @@
-import React, { FunctionComponent, useRef, useState, useEffect } from "react";
+import React, { FunctionComponent } from "react";
 import AutoSizer from "react-virtualized-auto-sizer";
 import { Box, makeStyles, useTheme } from "@material-ui/core";
-
-interface Beacon {
-  center: {
-    x: number;
-    y: number;
-  };
-  radius: number;
-  style: string;
-}
+import Particles from "react-tsparticles";
 
 const useStyles = makeStyles({
   root: {
@@ -22,60 +14,89 @@ const useStyles = makeStyles({
 export const Background: FunctionComponent = () => {
   const classes = useStyles();
   const {
-    palette: { primary, secondary },
+    palette: { primary },
   } = useTheme();
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [beacons, setBeacons] = useState<Beacon[]>([]);
-
-  useEffect(() => {
-    const id = setInterval(() => {
-      const context = canvasRef?.current?.getContext("2d");
-      if (canvasRef && canvasRef.current && context) {
-        context.lineWidth = 4;
-        context.clearRect(
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height
-        );
-        beacons.forEach(({ center: { x, y }, radius, style }) => {
-          context.strokeStyle = style;
-          context.beginPath();
-          context.ellipse(x, y, radius, radius, 0, 0, 2 * Math.PI);
-          context.stroke();
-        });
-        const updatedBeacons = beacons.map((beacon) => ({
-          ...beacon,
-          radius: beacon.radius + 1,
-        }));
-        const filteredBeacons = updatedBeacons.filter(
-          (beacon) => beacon.radius < 50
-        );
-        setBeacons(filteredBeacons);
-      }
-    }, 50);
-    return () => clearInterval(id);
-  });
-
-  const onMouseDown = (x: number, y: number) =>
-    setBeacons([
-      ...beacons,
-      {
-        center: { x, y },
-        radius: 0,
-        style: Math.random() < 0.5 ? primary.main : secondary.main,
-      },
-    ]);
 
   return (
     <Box className={classes.root}>
       <AutoSizer>
         {({ height, width }) => (
-          <canvas
-            ref={canvasRef}
-            height={height}
+          <Particles
             width={width}
-            onMouseDown={(event) => onMouseDown(event.clientX, event.clientY)}
+            height={height}
+            options={{
+              fpsLimit: 60,
+              interactivity: {
+                detectsOn: "canvas",
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  bubble: {
+                    distance: 400,
+                    duration: 2,
+                    opacity: 0.8,
+                    size: 40,
+                  },
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: primary.main,
+                },
+                links: {
+                  color: primary.main,
+                  distance: 150,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outMode: "bounce",
+                  random: false,
+                  speed: 6,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    value_area: 800,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  random: true,
+                  value: 5,
+                },
+              },
+              detectRetina: true,
+            }}
           />
         )}
       </AutoSizer>
